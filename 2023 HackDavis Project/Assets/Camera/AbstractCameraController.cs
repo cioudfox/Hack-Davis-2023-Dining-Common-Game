1c449dfa76351c6bfec348068c1be5d0371487e6
Young Cheol Ko <aqr1704@gmail.com>
2023-05-20 11:30:51 -0700

    Camera

---
+++ 2023 HackDavis Project/Assets/Camera/AbstractCameraController.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public abstract class AbstractCameraController : MonoBehaviour
+{
+    [SerializeField] protected GameObject Target;
+}
